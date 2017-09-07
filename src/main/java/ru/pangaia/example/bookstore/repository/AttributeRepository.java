d74f2df361f0bb4c4eb395aded4f1311ede51669
rb2o2 <kolbasa.sapiens@gmail.com>
2017-09-07 16:37:49 +0300

    fix

--- src/main/java/ru/pangaia/example/bookstore/repository/AttributeRepository.java
+++ src/main/java/ru/pangaia/example/bookstore/repository/AttributeRepository.java
@@ -17,21 +17,21 @@ public class AttributeRepository extends SimpleJpaRepository<Attribute, Long>
         super(Attribute.class, em);
     }
 
-    @Transactional
+    /*@Transactional
     public List<Attribute> findByNameIsLike(String substring)
     {
         return findAll()
                 .stream()
                 .filter((cat) -> cat.name.indexOf(substring) > 0)
                 .collect(Collectors.toList());
-    }
+    }*/
 
-    @Transactional
+    /*@Transactional
     public List<Attribute> findByParent(Attribute parent)
     {
         return findAll()
                 .stream()
                 .filter((cat) -> cat.parent == parent)
                 .collect(Collectors.toList());
-    }
+    }*/
 }
